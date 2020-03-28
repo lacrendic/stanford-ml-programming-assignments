54ed9638556fd1d2adfc1177bee0cab1f16c4d69
lacrendic <acrendic@gmail.com>
2020-03-28 20:41:50 -0300

    Part 4 of ex2: add costFunctionReg (regularized)

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -6,6 +6,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+n = length(theta);
 
 % You need to return the following variables correctly 
 J = 0;
@@ -17,11 +18,28 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+for i = 1:m
+        x_i = X(i, :);
+        y_i = y(i);
+        h_i = predict(x_i, theta);
+        J = J - y_i * log(h_i) - (1 - y_i) * log(1 - h_i);
+        for j = 1:length(grad)
+            grad(j) = grad(j) + (h_i - y_i) * x_i(j);
+        end
+    end
 
-
-
-
+    J = 1/m * J + lambda / (2 * m) * theta(2:n)' * theta(2:n);
+    grad = 1/m * grad;
+    for j = 2:n
+        grad(j) = grad(j) + lambda / m * theta(j);
+    end
 
 % =============================================================
 
+end
+
+
+
+function h = predict(x, theta)
+    h = sigmoid(x * theta);
 end
